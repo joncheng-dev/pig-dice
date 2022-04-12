4c26ef7d5d35eff350057d2609b20cdd7e540c0e
Jonathan Cheng <jcheng461@gmail.com>
2022-04-13 00:37:20 +0900

    add function to keep track of player turn. Upon clicking hold or end turn button, player changes from 1 to 2, or 2 to 1.

--- js/scripts.js
+++ js/scripts.js
@@ -6,14 +6,31 @@ function Player(id, roundPoints, gamePoints) {
   this.gamePoints = gamePoints;
 }
 
+function changePlayers(currentPlayer) {
+  switch (currentPlayer) {
+    case 1:
+      currentPlayer = 2;
+      console.log("It is now Player " + currentPlayer + "'s turn.");
+      break;
+    case 2:
+      currentPlayer = 1;
+      console.log("It is now Player " + currentPlayer + "'s turn.");
+      break;
+  }
+  return currentPlayer;
+}
+
 // User Interface Logic
 $(document).ready(function () {
   // Players initialize to start game.
   let playerOne = new Player("One", 0, 0);
   let playerTwo = new Player("Two", 0, 0);
 
+  let currentPlayer = 1;
+
   let roundScore = 0;
   let gameScore = 0;
+
   // Upon button click..
   $("#rollDie").click(function () {
     let roll = Math.floor(Math.random() * 6 + 1);
@@ -26,7 +43,8 @@ $(document).ready(function () {
       console.log("Score this round is " + roundScore);
     }
   });
-  $("#holdEndTurn").click(function () {
-    console.log("End turn button clicked");
+  // Click for current player to end turn.
+  $("#endTurn").click(function () {
+    currentPlayer = changePlayers(currentPlayer);
   });
 });
